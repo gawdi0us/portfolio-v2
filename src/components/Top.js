1d1b0dfec757eadba3bc0b487f1061db6ee79762
U-THEOPHILUS\Ron <ronald.hughes1982@gmail.com>
2020-03-09 01:29:41 -0400

    completed top nav area

--- src/components/Top.js
+++ src/components/Top.js
@@ -5,35 +5,48 @@ import Linkedlogo from '../img/linkedin-icon.png'
 
 
 export const Top = () => {
-
+    // STYLES
     const topStyle = {
         display: "flex",
         justifyContent: "space-between",
-        backgroundColor: "#272259",
-        height: "130px",
+        backgroundColor: "#084887",
+        height: "10rem",
         width: "100%",
         fontFamily: 'BioRhyme',
-        fontSize: '1.4rem',
+        fontSize: '2rem',
         color: 'white',
-        alignItems: 'center'
+        alignItems: 'center',
+        borderBottom: "9px solid whitesmoke"
     }
 
     const brandStyle = {
-        width: "20rem",
-        marginLeft: "2rem",
-    
+        width: "30rem", 
+        // marginTop: "3rem",
+        marginLeft: "3rem",
+    }
+    const titleStyle = {
+        marginTop: "-3rem",
+    }
+
+    const img = {
+        marginRight: "40px"
     }
     
     return (
      
         <div className="container">
+            {/* Navigation */}
             <div className="top" style={topStyle}>
                 <div className="brand" style={brandStyle}>
-                <p>Ron Hughes <br /> 
-                Full Stack Web Developer</p>
+                <p>ronhughes.dev</p>
+                <div className="title" style={titleStyle}>
+                <h5>Full-Stack Web Developer</h5>
+                </div> 
                 </div>
-                <div className="social-icons"><p><img src={Linkedlogo} /> <img src={Gitlogo} /></p></div>
+                <div className="social-icons"><p><img src={Linkedlogo} style={img} /> <img src={Gitlogo} /></p></div>
             </div>
+
+           
             
 
 
